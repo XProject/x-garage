9dfa842a1651e2363f459abecf3a12ef32eabfd3
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-03-20 00:59:31 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center"><b>X-GARAGE (WIP)</b></h1>
+
+<h3 align="center">Garage System with GTA IPLs for FiveM's OneSync Infinity</h3>
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game        "gta5"
 name        "x-garage"
 version     "0.0.0"
 repository  "https://github.com/XProject/x-garage"
-description "Project-X Garage: Garage System for FiveM's OneSync Infinity with GTA ipls"
+description "Project-X Garage: Garage System with GTA IPLs for FiveM's OneSync Infinity"
 
 shared_scripts {
     "shared/*.lua",
